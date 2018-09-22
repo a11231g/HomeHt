1e6ee431d2905ceb63f29408832ca632628ec5a6
Ali Atik <ali1wg@gmail.com>
2018-09-23 00:47:49 +0330

    added: splash screen to android

--- android/app/src/main/java/com/homeht/MainActivity.java
+++ android/app/src/main/java/com/homeht/MainActivity.java
@@ -1,5 +1,5 @@
 package com.homeht;
-
+import com.reactnativecomponent.splashscreen.RCTSplashScreen;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -9,6 +9,11 @@ public class MainActivity extends ReactActivity {
      * This is used to schedule rendering of the component.
      */
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        RCTSplashScreen.openSplashScreen(this);   //open splashscreen
+        //RCTSplashScreen.openSplashScreen(this, true, ImageView.ScaleType.FIT_XY);   //open splashscreen fullscreen
+        super.onCreate(savedInstanceState);
+    }
     protected String getMainComponentName() {
         return "homeHt";
     }
